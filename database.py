5664120d257db9286a3d3650fdff4591a93b302d
Andres Uribe G. <neluri@comfenalcoquindio.co>
2024-02-28 09:04:39 -0500

    requirements

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine=create_engine("postgresql://postgres:system@localhost/item_db", echo=True)
+engine=create_engine("postgresql://postgres:clave@localhost/item_db", echo=True)
 
 Base=declarative_base()
 
